4a8fd011c61877a72cdc9fbab8229b82403c2946
SaraM <ikaqh@plaiaundi.net>
2021-01-26 23:05:02 +0100

    datos balizas

--- app/Http/Controllers/datosBalizas.php
+++ app/Http/Controllers/datosBalizas.php
@@ -13,7 +13,7 @@ class datosBalizas extends Controller {
         $curl = curl_init();
 
         curl_setopt($curl, CURLOPT_URL, "https://euskalmet.beta.euskadi.eus/vamet/stations/stationList/stationList.json");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
         $response = utf8_encode(curl_exec($curl));
@@ -27,15 +27,15 @@ class datosBalizas extends Controller {
 
                     $curl2 = curl_init();
 
-                curl_setopt($curl2, CURLOPT_URL, "https://euskalmet.beta.euskadi.eus/vamet/stations/readings/".$nomBaliza."/".$fecha."readingsData.json");
-                curl_setopt($curl2, CURLOPT_RETURNTRANSFER,true);
-                curl_setopt($curl2, CURLOPT_SSL_VERIFYPEER, false);
+                    curl_setopt($curl2, CURLOPT_URL, "https://euskalmet.beta.euskadi.eus/vamet/stations/readings/" . $nomBaliza . "/" . $fecha . "readingsData.json");
+                    curl_setopt($curl2, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($curl2, CURLOPT_SSL_VERIFYPEER, false);
 
-                $response = utf8_encode(curl_exec($curl2));
-                curl_close($curl2);
+                    $response = utf8_encode(curl_exec($curl2));
+                    curl_close($curl2);
 
-                $datosBalizas = json_decode($response, true);
-                echo var_dump($datosBalizas);
+                    $datosBalizas = json_decode($response, true);
+                    echo var_dump($datosBalizas);
 
                 }
             }
